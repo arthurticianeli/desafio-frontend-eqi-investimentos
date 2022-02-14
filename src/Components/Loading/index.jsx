d47b977c90f69b5358930fda0877eb5c270654ab
arthurticianeli <arthurpeixoto@gmail.com>
2022-02-14 14:25:52 -0300

    feat: changed the loading component to skeleton

--- src/Components/Loading/index.jsx
+++ src/Components/Loading/index.jsx
@@ -1,18 +1,35 @@
-import { Flex, Heading, Spinner } from '@chakra-ui/react';
+import { Box, Center, Flex, Skeleton, Stack } from '@chakra-ui/react';
 import React from 'react';
 
 function Loading() {
   return (
-    <Flex flexDir={'column'} alignItems={'center'} m="auto" h="40vh">
-      <Heading size={'lg'}>Carregando dados</Heading>
-      <Spinner
-        thickness="4px"
-        speed="0.65s"
-        emptyColor="gray.300"
-        color="primary"
-        size="xl"
-      />
-    </Flex>
+    <Stack>
+      <Skeleton height="50px" width={'150px'} />
+      <Flex flexWrap={'wrap'} justifyContent={'center'}>
+        <Box mr={{ base: '0px', sm: '20px' }}>
+          <Skeleton height="100px" width={'250px'} />
+          <Skeleton height="80px" width={'250px'} />
+          <Skeleton height="80px" width={'250px'} />
+          <Skeleton height="80px" width={'250px'} />
+        </Box>
+        <Box>
+          <Skeleton height="100px" width={'250px'} />
+          <Skeleton height="80px" width={'250px'} />
+          <Skeleton height="80px" width={'250px'} />
+          <Skeleton height="80px" width={'250px'} />
+        </Box>
+      </Flex>
+      <Center>
+        <Flex flexWrap={'wrap'} justifyContent={'center'}>
+          <Skeleton
+            height="80px"
+            width={'200px'}
+            mr={{ base: '0px', sm: '20px' }}
+          />
+          <Skeleton height="80px" width={'200px'} />
+        </Flex>
+      </Center>
+    </Stack>
   );
 }
 
